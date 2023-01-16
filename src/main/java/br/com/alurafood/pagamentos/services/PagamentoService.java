fec3b423c1304e00b6de24c00be1d1360f0b006e
rookie-leo <proleonarpa@gmail.com>
2023-01-16 13:27:01 -0300

    Implementado o service

---
+++ src/main/java/br/com/alurafood/pagamentos/services/PagamentoService.java
@@ -0,0 +1,52 @@
+package br.com.alurafood.pagamentos.services;
+
+import br.com.alurafood.pagamentos.dto.PagamentoDto;
+import br.com.alurafood.pagamentos.models.Pagamento;
+import br.com.alurafood.pagamentos.models.enums.Status;
+import br.com.alurafood.pagamentos.repositories.PagamentoRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public class PagamentoService {
+
+    private PagamentoRepository repository;
+
+    private ModelMapper modelMapper;
+
+    public Page<PagamentoDto> obterTodos(Pageable paginacao) {
+        return repository
+                .findAll(paginacao)
+                .map(p -> modelMapper
+                        .map(p, PagamentoDto.class));
+    }
+
+    public PagamentoDto obterPorId(Long id) {
+        Pagamento pagamento = repository.findById(id).orElseThrow(() ->
+                new EntityNotFoundException()
+        );
+
+        return modelMapper.map(pagamento, PagamentoDto.class);
+    }
+
+    public PagamentoDto criarPagamento(PagamentoDto dto) {
+        Pagamento pagamento = modelMapper.map(dto, Pagamento.class);
+        pagamento.setStatus(Status.CRIADO);
+        repository.save(pagamento);
+
+        return modelMapper.map(pagamento, PagamentoDto.class);
+    }
+
+    public PagamentoDto atualizarPagamento(Long id, PagamentoDto dto) {
+        Pagamento pagamento = modelMapper.map(dto, Pagamento.class);
+        pagamento.setId(id);
+        pagamento = repository.save(pagamento);
+        return modelMapper.map(pagamento, PagamentoDto.class);
+    }
+
+    public void excluirPagamento(Long id) {
+        repository.deleteById(id);
+    }
+
+}
